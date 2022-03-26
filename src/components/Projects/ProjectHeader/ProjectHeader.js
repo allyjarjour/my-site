cee89c7bcceaf803a124103fd293fc64e72bf5f4
Ally Jarjour <allyjarjour@gmail.com>
2022-03-25 21:42:19 -0700

    Add GitHub logo to ProjectHeader

--- src/components/Projects/ProjectHeader/ProjectHeader.js
+++ src/components/Projects/ProjectHeader/ProjectHeader.js
@@ -1,20 +1,16 @@
 import { CardHeader, Typography } from '@mui/material';
 import React from 'react'
+import GitHubLogo from '../../GitHubLogo';
 import './projectHeader.scss'
 
-export default function ProjectHeader({title}) {
+export default function ProjectHeader({title, gitHubLink}) {
   return (
     <CardHeader
       className="project-header"
       title={
         <div className="title">
           <Typography variant="card-title">{title}</Typography>
-          <img
-            className="logo"
-            aria-label="See project on GitHub"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-            alt="GitHub logo"
-          />
+          <GitHubLogo href={gitHubLink} />
         </div>
       }
     />
